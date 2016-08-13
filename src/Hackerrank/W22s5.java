d06c780189a358c4e0e5db4b3fd03345a8a20672
Klaudioz <klaudioz@gmail.com>
2016-08-13 02:08:03 -0500

    It's needed an optimization

--- src/Hackerrank/W22s5.java
+++ src/Hackerrank/W22s5.java
@@ -3,22 +3,23 @@ package Hackerrank;
 import java.io.*;
 import java.util.*;
 
+@SuppressWarnings("unchecked")
 public class W22s5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Set<Integer> set = new HashSet<Integer>();
         Set<Set<Integer>> sets = new HashSet<Set<Integer>>();
-        Map<Set<Integer>, Integer> map = new HashMap<>();
+        Map<Set<Integer>, Long> map = new HashMap<>();
         int n = sc.nextInt(); //size set U
         for (int i = 1; i <= n; i++)
             set.add(i);
         sets = powerSet(set);
         for (Set varSet : sets)
-            map.put(varSet,0);
+            map.put(varSet,0L);
         int times = sc.nextInt(); //number queries
         for (int i = 0; i < times; i++) {
-            int numberCase1;
+            long numberCase1;
             String numberCase2;
             char[] charArray = new char[16];
             Set<Integer> tempSet = new HashSet<Integer>();
@@ -26,7 +27,7 @@ public class W22s5 {
             int type = sc.nextInt();
             switch (type) {
                 case 1:
-                    numberCase1 = sc.nextInt(); //value
+                    numberCase1 = sc.nextLong(); //value
                     numberCase2 = sc.next(); //set binary representation
                     charArray = numberCase2.toCharArray();
                     for(int k=0;k<charArray.length;k++){
@@ -41,7 +42,7 @@ public class W22s5 {
                     //System.out.println(map);
                     break;
                 case 2:
-                    numberCase1 = sc.nextInt(); //value
+                    numberCase1 = sc.nextLong(); //value
                     numberCase2 = sc.next(); //set binary representation
                     charArray = numberCase2.toCharArray();
                     for(int k=0;k<charArray.length;k++){
